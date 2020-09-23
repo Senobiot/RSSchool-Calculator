df5ca57ca711bb9ba69322b27016973d0bd3e941
Aliaksandr Kulik <inter_net@tut.by>
2020-09-23 22:21:14 +0300

    feat: add putial functions

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -5,27 +5,59 @@ let numBtn = document.querySelectorAll(".num"),
 	c = document.querySelector(".clear"),
 	ac = document.querySelector(".all_clear"),
 	negativeBtn = document.querySelector(".negative"),
-	memoryCurrent = "0",
-	memoryNewNumber = false,
+	display = document.querySelector(".calculator_screen"),
+	memoryCurrent = 0,
+	oper = "",
+	// memoryNewNumber = false,
 	memoryPendingOperation = "";
-//console.log()
 
 for (let i = 0; i < numBtn.length; i++) {
 	let number = numBtn[i];
-	number.addEventListener('click', function(e){
-		numberPress(e.target.textContent)
-	})
+	number.addEventListener('click', numberPress)
 };
+
 for (let i = 0; i < operationBtn.length; i++) {
 	let operation = operationBtn[i];
-	operation.addEventListener('click', function (e){
-		operationPress(e.target.textContent)
-	})
+	operation.addEventListener('click', operationPress)
 };
 
-decimalBtn.addEventListener('click', decimal);
 
-resultBtn.addEventListener('click', result);
+function numberPress() {
+	console.log(this.attr);
+	if (+display.innerHTML === 0) {
+		display.innerHTML = this.innerHTML
+	} else {
+		if (!memoryPendingOperation) {
+			display.innerHTML += this.innerHTML;
+		} else {
+			memoryCurrent = display.innerHTML;
+			oper = memoryPendingOperation;
+			memoryPendingOperation = 0;
+			display.innerHTML = this.innerHTML;
+		}
+	}
+}
+
+function operationPress() {
+	memoryPendingOperation = this.innerHTML;
+	if (oper === "+") {
+		display.innerHTML = +display.innerHTML + +memoryCurrent;
+		oper = "";
+	} else if (oper === "*") {
+		display.innerHTML = +display.innerHTML*(+memoryCurrent);
+		oper = "";
+	} else if (oper === "/") {
+		display.innerHTML = +memoryCurrent / +display.innerHTML ;
+		oper = "";
+	} else if (oper === "-") {
+		display.innerHTML = +memoryCurrent - +display.innerHTML ;
+		oper = "";
+	} else {
+
+	}
+}
+
+decimalBtn.addEventListener('click', decimal);
 
 c.addEventListener('click', function(e){
 		clear(e.srcElement.classList[1])
@@ -35,25 +67,17 @@ ac.addEventListener('click', function(e){
 		clear(e.srcElement.classList[1])
 	});
 
-negativeBtn.addEventListener('click', negative);
+negativeBtn.addEventListener('click', function(){
+	display.innerHTML = - display.innerHTML
+});
 
 
 
-function numberPress (symbol) {
-	console.log(`click ${symbol}`)
-}
-function operationPress(symbol) {
-	console.log(`click ${symbol}`)
-}
+
 function decimal(argument) {
 	console.log('click decimal button')
 }
 function clear(classList) {
-	console.log(`click ${classList}`);
-}
-function result(argument) {
-	console.log('click result button');
-}
-function negative(argument) {
-	console.log('click negative button');
+	display.innerHTML = 0;
+	memoryCurrent = 0;
 }
